b67f54a1d23070809440264ed1387bdcddd000c2
unknown <rv8828120@gmail.com>
2023-04-27 02:58:23 +0800

    Pseudocode

--- Multiple File.py
+++ Multiple File.py
@@ -1,3 +1,12 @@
 #Vargas, Ruben A.
 #BSCPE 1-4
 #Object-Oriented Programming
+
+#To start writing, open the "mylife.txt" file.
+    #Loop until the user decides to end it.
+        #Request to the user to enteer a line of text.
+        #Add the text line, then a new line character, to the file.
+        #Ask the user if they want to enter another line of text.
+        #Continue the loop if the user(s) enter "y" to prompt for another line of text.
+        #Break the loop if the user types "n" to finish the program.
+#Close the file
